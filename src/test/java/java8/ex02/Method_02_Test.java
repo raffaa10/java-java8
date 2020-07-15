4c3ae66ce1ebe535274901d1964497f29a476cde
33769 <33769@LAPTOP-EG5OS6FD.home>
2020-07-15 12:04:29 +0200

    tp - redéfinition d'une méthode

--- src/test/java/java8/ex02/Method_02_Test.java
+++ src/test/java/java8/ex02/Method_02_Test.java
@@ -41,7 +41,10 @@ public class Method_02_Test {
         // TODO la méthode retourne une chaîne de la forme DaoA[<nb_personnes> persons]
         // TODO exemple de résultat : "DaoA[14 persons]", "DaoA[30 persons]"
         // TODO l'implémentation réutilise la méthode format() de l'interface
-
+        
+        public String format() {
+        	return findAll().size() + " persons";
+        }
     }
     // end::DaoA[]
 
@@ -53,6 +56,6 @@ public class Method_02_Test {
         // TODO invoquer la méthode format() pour que le test soit passant
         String result = daoA.format();
 
-        "DaoA[20 persons]".equals(result);
+        assert "DaoA[20 persons]".equals(result);
     }
 }
