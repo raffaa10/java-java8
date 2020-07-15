e130447cf0f11dcf26d6dca76a3c0222c1bd7c93
33769 <33769@LAPTOP-EG5OS6FD.home>
2020-07-15 10:49:02 +0200

    TP - public default méthode

--- src/test/java/java8/ex02/Method_02_Test.java
+++ src/test/java/java8/ex02/Method_02_Test.java
@@ -19,6 +19,11 @@ public class Method_02_Test {
         // TODO créer une méthode String format()
         // TODO la méthode retourne une chaîne de la forme [<nb_personnes> persons]
         // TODO exemple de résultat : "[14 persons]", "[30 persons]"
+        
+        default String format() {
+        	return findAll().size() + " persons";
+        	
+        }
     }
     // end::IDao[]
 
@@ -46,7 +51,7 @@ public class Method_02_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode format() pour que le test soit passant
-        String result = null;
+        String result = daoA.format();
 
         "DaoA[20 persons]".equals(result);
     }
