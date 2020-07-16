008d92e3426759391a9537b97b3cd1fe6f2575c5
33769 <33769@LAPTOP-EG5OS6FD.home>
2020-07-16 15:53:27 +0200

    TP functions v0

--- src/test/java/java8/ex05/Function_05_Test.java
+++ src/test/java/java8/ex05/Function_05_Test.java
@@ -19,7 +19,7 @@ public class Function_05_Test {
 
     // TODO compléter la fonction
     // TODO vérifier que l'age > 4 avec une assertion JUnit
-    Consumer<Person> verifyAge = p -> p.getAge() > 4;
+    //Consumer<Person> verifyAge = p -> p.getAge() > 4;
 
     // TODO compléter la fonction
     // TODO vérifier que le mot de passe est "secret" avec une assertion JUnit
@@ -33,13 +33,14 @@ public class Function_05_Test {
 
         // TODO invoquer la méthode personList.forEach pour modifier les mots de passe en "secret"
         // personList.forEach...
+        
 
         // TODO remplacer la boucle for par l'invocation de la méthode forEach
         // TODO Utiliser la méthode andThen pour chaîner les vérifications verifyAge et verifyPassword
         // personList.forEach...
-        for(Person p : personList) {
-            verifyAge.accept(p);
-            verifyPassword.accept(p);
+//        for(Person p : personList) {
+//            verifyAge.accept(p);
+//            verifyPassword.accept(p);
         }
-    }
+  
 }
